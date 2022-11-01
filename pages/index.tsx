55296b0e44fff470cbdc4e51181210d2da5a23c9
Matheus Barboza <matheus.barboza102@gmail.com>
2022-11-01 12:38:30 -0300

    testando server side rendering

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,29 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
-export default function Home() {
+interface HomeProps {
+  count: number;
+}
+
+export default function Home(props: HomeProps) {
+  console.log(props.count)
   return (
     <div>
       <Head>
         <title>Bolão da Copa</title>
       </Head>
 
-      <h1>Hello World!</h1>
+      <h1>Count: {props.count}</h1>
     </div>
   )
+}
+
+export const getServerSideProps = async () => {
+  const res = await fetch('http://localhost:3333/pools/count')
+  const data = await res.json()
+
+  return {
+    props: {
+      count: data.count,
+    }
+  }
 }
